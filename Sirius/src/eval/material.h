fe4aed093db98a6e2362c9cef6b3fc1cc48ae267
AspectOfTheNoob <Mcthouacbb@gmail.com>
2023-07-15 09:05:00 -0400

    Added more material + PSQT tuning

--- Sirius/src/eval/material.h
+++ Sirius/src/eval/material.h
@@ -5,17 +5,17 @@
 namespace eval
 {
 
-constexpr int QUEEN_VALUE_MG = 2006;
-constexpr int ROOK_VALUE_MG = 904;
-constexpr int BISHOP_VALUE_MG = 703;
-constexpr int KNIGHT_VALUE_MG = 638;
-constexpr int PAWN_VALUE_MG = 176;
+constexpr int QUEEN_VALUE_MG = 1993;
+constexpr int ROOK_VALUE_MG = 932;
+constexpr int BISHOP_VALUE_MG = 734;
+constexpr int KNIGHT_VALUE_MG = 668;
+constexpr int PAWN_VALUE_MG = 180;
 
-constexpr int QUEEN_VALUE_EG = 1821;
-constexpr int ROOK_VALUE_EG = 1027;
-constexpr int BISHOP_VALUE_EG = 567;
+constexpr int QUEEN_VALUE_EG = 1882;
+constexpr int ROOK_VALUE_EG = 1033;
+constexpr int BISHOP_VALUE_EG = 568;
 constexpr int KNIGHT_VALUE_EG = 515;
-constexpr int PAWN_VALUE_EG = 256;
+constexpr int PAWN_VALUE_EG = 257;
 
 
 int getPieceValueMG(PieceType piece);
