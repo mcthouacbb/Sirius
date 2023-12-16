f839f24806fab840cbb8db773f0811ae35b1c43c
AspectOfTheNoob <Mcthouacbb@gmail.com>
2023-12-16 11:36:36 -0500

    Zobrist hashing stuff

--- Sirius/src/zobrist.h
+++ Sirius/src/zobrist.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
 #include "defs.h"
 
 namespace zobrist
 {
+
+struct Keys
+{
+    uint64_t blackToMove;
+    std::array<std::array<std::array<uint64_t, 64>, 6>, 2> pieceSquares;
+    std::array<uint64_t, 16> castlingRights;
+    std::array<uint64_t, 8> epFiles;
+};
+
+extern Keys keys;
 
 void init();
 
@@ -25,3 +36,33 @@ struct ZKey
     bool operator==(const ZKey& other) const = default;
     bool operator!=(const ZKey& other) const = default;
 };
+
+inline void ZKey::flipSideToMove()
+{
+    value ^= zobrist::keys.blackToMove;
+}
+
+inline void ZKey::addPiece(PieceType piece, Color color, uint32_t square)
+{
+    value ^= zobrist::keys.pieceSquares[static_cast<int>(color)][static_cast<int>(piece) - 1][square];
+}
+
+inline void ZKey::removePiece(PieceType piece, Color color, uint32_t square)
+{
+    value ^= zobrist::keys.pieceSquares[static_cast<int>(color)][static_cast<int>(piece) - 1][square];
+}
+
+inline void ZKey::movePiece(PieceType piece, Color color, uint32_t src, uint32_t dst)
+{
+    value ^= zobrist::keys.pieceSquares[static_cast<int>(color)][static_cast<int>(piece) - 1][src] ^ zobrist::keys.pieceSquares[static_cast<int>(color)][static_cast<int>(piece) - 1][dst];
+}
+
+inline void ZKey::updateCastlingRights(uint32_t castlingRights)
+{
+    value ^= zobrist::keys.castlingRights[castlingRights];
+}
+
+inline void ZKey::updateEP(uint32_t epFile)
+{
+    value ^= zobrist::keys.epFiles[epFile];
+}
