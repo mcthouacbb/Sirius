27c41409a070da1dfc521e6b7845d24bf436e697
AspectOfTheNoob <Mcthouacbb@gmail.com>
2023-12-16 10:44:11 -0500

    Make combined_psqt inline/header-only

--- Sirius/src/eval/combined_psqt.h
+++ Sirius/src/eval/combined_psqt.h
@@ -1,11 +1,32 @@
 #pragma once
 
 #include "../defs.h"
+#include "combined_psqt.h"
+#include "eval_constants.h"
+
+#include <array>
 
 namespace eval
 {
 
-PackedScore combinedPsqtScore(Color color, PieceType piece, int square);
+constexpr inline auto init()
+{
+    std::array<std::array<std::array<PackedScore, 64>, 7>, 2> combined = {};
+    for (int piece = 1; piece < 7; piece++)
+        for (int sq = 0; sq < 64; sq++)
+        {
+            combined[0][piece][sq] = (MATERIAL[piece] + PSQT[piece][sq ^ 0b111000]);
+            combined[1][piece][sq] = -(MATERIAL[piece] + PSQT[piece][sq]);
+        }
+    return combined;
+}
+
+constexpr auto combinedPsqt = init();
+
+inline PackedScore combinedPsqtScore(Color color, PieceType piece, int square)
+{
+    return combinedPsqt[static_cast<int>(color)][static_cast<int>(piece)][square];
+}
 
 
 }
