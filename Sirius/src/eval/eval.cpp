5804db5a9f6b2f2d41f256ba2926d2103475d457
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-02-20 17:58:30 -0500

    Added rook on open/semi-open file Terrible impl, desperately needs refactor Would not be surprised if I somehow messed it up Bench: 9675516

--- Sirius/src/eval/eval.cpp
+++ Sirius/src/eval/eval.cpp
@@ -14,6 +14,38 @@ int rawEval(const Board& board)
 {
     Color color = board.sideToMove();
     PackedScore eval = board.evalState().materialPsqt;
+
+    std::array<Bitboard, 2> pawns = {
+        board.getPieces(Color::WHITE, PieceType::PAWN),
+        board.getPieces(Color::BLACK, PieceType::PAWN)
+    };
+
+    Bitboard rooks = board.getPieces(Color::WHITE, PieceType::ROOK);
+    while (rooks)
+    {
+        uint32_t sq = rooks.poplsb();
+        if ((pawns[0] & (FILE_A << (sq % 8))).empty())
+        {
+            if ((pawns[1] & (FILE_A << (sq % 8))).empty())
+                eval += OPEN_ROOK[0];
+            else
+                eval += OPEN_ROOK[1];
+        }
+    }
+
+    rooks = board.getPieces(Color::BLACK, PieceType::ROOK);
+    while (rooks)
+    {
+        uint32_t sq = rooks.poplsb();
+        if ((pawns[1] & (FILE_A << (sq % 8))).empty())
+        {
+            if ((pawns[0] & (FILE_A << (sq % 8))).empty())
+                eval -= OPEN_ROOK[0];
+            else
+                eval -= OPEN_ROOK[1];
+        }
+    }
+
     if (board.getPieces(Color::WHITE, PieceType::BISHOP).popcount() >= 2)
         eval += BISHOP_PAIR;
     if (board.getPieces(Color::BLACK, PieceType::BISHOP).popcount() >= 2)
