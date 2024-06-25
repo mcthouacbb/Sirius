92c8be7e17ca61b80498393a17e8daae1228dfe2
mcthouacbb <mcthouacbb@gmail.com>
2024-06-25 15:40:11 +0000

    2370/5000 iterations Bench: 4306024

--- Sirius/src/search_params.h
+++ Sirius/src/search_params.h
@@ -39,58 +39,58 @@ void updateLmrTable();
 #define SEARCH_PARAM_CALLBACK(name, val, min, max, step, callback) SEARCH_PARAM(name, val, min, max, step)
 #endif
 
-SEARCH_PARAM(hardTimeScale, 50, 20, 100, 5);
-SEARCH_PARAM(softTimeScale, 60, 30, 100, 5);
-SEARCH_PARAM(incrementScale, 75, 25, 100, 5);
-SEARCH_PARAM(baseTimeScale, 20, 8, 40, 1);
+SEARCH_PARAM(hardTimeScale, 45, 20, 100, 5);
+SEARCH_PARAM(softTimeScale, 61, 30, 100, 5);
+SEARCH_PARAM(incrementScale, 78, 25, 100, 5);
+SEARCH_PARAM(baseTimeScale, 19, 8, 40, 1);
 
-SEARCH_PARAM(nodeTMBase, 135, 110, 200, 5);
-SEARCH_PARAM(nodeTMScale, 150, 100, 200, 5);
+SEARCH_PARAM(nodeTMBase, 143, 110, 200, 5);
+SEARCH_PARAM(nodeTMScale, 152, 100, 200, 5);
 
-SEARCH_PARAM(maxHistBonus, 1896, 1024, 3072, 128);
-SEARCH_PARAM(histScaleQuadratic, 4, 1, 8, 1);
-SEARCH_PARAM(histScaleLinear, 120, 64, 384, 16);
-SEARCH_PARAM(histBonusOffset, 120, 64, 768, 32);
+SEARCH_PARAM(maxHistBonus, 1945, 1024, 3072, 128);
+SEARCH_PARAM(histScaleQuadratic, 5, 1, 8, 1);
+SEARCH_PARAM(histScaleLinear, 155, 64, 384, 16);
+SEARCH_PARAM(histBonusOffset, 181, 64, 768, 32);
 
 SEARCH_PARAM(aspInitDelta, 15, 8, 30, 1);
-SEARCH_PARAM(minAspDepth, 5, 3, 7, 1);
-SEARCH_PARAM(aspWideningFactor, 8, 1, 32, 2);
+SEARCH_PARAM(minAspDepth, 4, 3, 7, 1);
+SEARCH_PARAM(aspWideningFactor, 7, 1, 32, 2);
 
-SEARCH_PARAM(minIIRDepth, 4, 2, 9, 1);
+SEARCH_PARAM(minIIRDepth, 2, 2, 9, 1);
 
-SEARCH_PARAM(rfpMaxDepth, 8, 4, 10, 1);
-SEARCH_PARAM(rfpImprovingMargin, 50, 30, 80, 3);
-SEARCH_PARAM(rfpMargin, 80, 50, 100, 3);
-SEARCH_PARAM(rfpHistDivisor, 400, 256, 512, 10);
+SEARCH_PARAM(rfpMaxDepth, 6, 4, 10, 1);
+SEARCH_PARAM(rfpImprovingMargin, 48, 30, 80, 3);
+SEARCH_PARAM(rfpMargin, 79, 50, 100, 3);
+SEARCH_PARAM(rfpHistDivisor, 381, 256, 512, 10);
 
 SEARCH_PARAM(nmpMinDepth, 2, 2, 5, 1);
-SEARCH_PARAM(nmpBaseReduction, 3, 2, 5, 1);
+SEARCH_PARAM(nmpBaseReduction, 4, 2, 5, 1);
 SEARCH_PARAM(nmpDepthReductionScale, 3, 3, 6, 1);
-SEARCH_PARAM(nmpEvalReductionScale, 200, 50, 300, 10);
+SEARCH_PARAM(nmpEvalReductionScale, 197, 50, 300, 10);
 SEARCH_PARAM(nmpMaxEvalReduction, 3, 2, 5, 1);
 
-SEARCH_PARAM(fpBaseMargin, 120, 60, 360, 8);
-SEARCH_PARAM(fpDepthMargin, 75, 10, 180, 8);
+SEARCH_PARAM(fpBaseMargin, 114, 60, 360, 8);
+SEARCH_PARAM(fpDepthMargin, 74, 10, 180, 8);
 SEARCH_PARAM(fpMaxDepth, 6, 4, 9, 1);
 
 SEARCH_PARAM(lmpMaxDepth, 8, 4, 11, 1);
-SEARCH_PARAM(lmpMinMovesBase, 3, 2, 7, 1);
+SEARCH_PARAM(lmpMinMovesBase, 2, 2, 7, 1);
 
-SEARCH_PARAM(maxSeePruneDepth, 8, 6, 11, 1);
-SEARCH_PARAM(seePruneMarginNoisy, -100, -120, -30, 5);
-SEARCH_PARAM(seePruneMarginQuiet, -60, -120, -30, 5);
+SEARCH_PARAM(maxSeePruneDepth, 9, 6, 11, 1);
+SEARCH_PARAM(seePruneMarginNoisy, -107, -120, -30, 5);
+SEARCH_PARAM(seePruneMarginQuiet, -63, -120, -30, 5);
 
-SEARCH_PARAM(maxHistPruningDepth, 4, 2, 8, 1);
-SEARCH_PARAM(histPruningMargin, 1536, 512, 4096, 128);
+SEARCH_PARAM(maxHistPruningDepth, 6, 2, 8, 1);
+SEARCH_PARAM(histPruningMargin, 1546, 512, 4096, 128);
 
 SEARCH_PARAM(lmrMinDepth, 3, 2, 5, 1);
-SEARCH_PARAM(lmrMinMovesNonPv, 3, 1, 6, 1);
+SEARCH_PARAM(lmrMinMovesNonPv, 2, 1, 6, 1);
 SEARCH_PARAM(lmrMinMovesPv, 5, 2, 8, 1);
 SEARCH_PARAM(lmrFailHighCountMargin, 4, 2, 12, 1);
 
-SEARCH_PARAM_CALLBACK(lmrBase, 77, -50, 200, 10, updateLmrTable);
-SEARCH_PARAM_CALLBACK(lmrDivisor, 236, 180, 320, 10, updateLmrTable);
-SEARCH_PARAM(lmrHistDivisor, 8192, 4096, 16384, 512);
+SEARCH_PARAM_CALLBACK(lmrBase, 88, -50, 200, 10, updateLmrTable);
+SEARCH_PARAM_CALLBACK(lmrDivisor, 224, 180, 320, 10, updateLmrTable);
+SEARCH_PARAM(lmrHistDivisor, 7844, 4096, 16384, 512);
 
 
 }
