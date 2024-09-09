e6d715bb88e9edd2b893eca9263ceac37a13f4b8
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-09-09 19:59:57 -0400

    NMP phase reduction Bench: 6234332

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -418,7 +418,11 @@ int Search::search(SearchThread& thread, int depth, SearchStack* stack, int alph
             stack->eval >= beta && stack->staticEval >= beta + nmpEvalBaseMargin - nmpEvalDepthMargin * depth &&
             nonPawns.multiple())
         {
-            int r = nmpBaseReduction + depth / nmpDepthReductionScale + std::min((stack->eval - beta) / nmpEvalReductionScale, nmpMaxEvalReduction);
+            int r =
+                nmpBaseReduction +
+                depth / nmpDepthReductionScale +
+                std::min((stack->eval - beta) / nmpEvalReductionScale, nmpMaxEvalReduction) +
+                (24 - std::min(thread.evalState.phase(), 24)) / 8;
             board.makeNullMove();
             rootPly++;
             int nullScore = -search(thread, depth - r, stack + 1, -beta, -beta + 1, false, !cutnode);
