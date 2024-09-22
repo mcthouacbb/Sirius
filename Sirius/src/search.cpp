ace4e1914d35f31c3a6acb1c5342244d93990f55
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-09-22 12:16:29 -0400

    rfp tweak Bench: 5814230

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -406,7 +406,7 @@ int Search::search(SearchThread& thread, int depth, SearchStack* stack, int alph
     if (!pvNode && !inCheck && !excluded)
     {
         // reverse futility pruning
-        if (depth <= rfpMaxDepth && stack->eval >= beta + (improving ? rfpImprovingMargin : rfpMargin) * depth + stack[-1].histScore / rfpHistDivisor)
+        if (depth <= rfpMaxDepth && stack->eval >= beta && stack->eval >= beta + rfpMargin * (depth - improving) + stack[-1].histScore / rfpHistDivisor)
             return stack->eval;
 
         if (depth <= razoringMaxDepth && stack->eval <= alpha - razoringMargin * depth && alpha < 2000)
