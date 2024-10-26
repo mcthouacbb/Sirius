6a087408f39f35e3de12ba10bf0b491d66ea88ef
AspectOfTheNoob <Mcthouacbb@gmail.com>
2024-10-26 11:37:11 -0400

    SPSA 1596 iters Bench: 5747180

--- Sirius/src/search_params.h
+++ Sirius/src/search_params.h
@@ -5,8 +5,6 @@
 #include <deque>
 #include <functional>
 
-#define EXTERNAL_TUNE
-
 namespace search
 {
 
@@ -59,13 +57,13 @@ SEARCH_PARAM(histMalusQuadratic, 5, 1, 8, 1);
 SEARCH_PARAM(histMalusLinear, 243, 64, 384, 32);
 SEARCH_PARAM(histMalusOffset, 66, 64, 768, 64);
 
-SEARCH_PARAM(pawnCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(materialCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(nonPawnStmCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(nonPawnNstmCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(threatsCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(minorCorrWeight, 256, 96, 768, 64);
-SEARCH_PARAM(majorCorrWeight, 256, 96, 768, 64);
+SEARCH_PARAM(pawnCorrWeight, 295, 96, 768, 64);
+SEARCH_PARAM(materialCorrWeight, 292, 96, 768, 64);
+SEARCH_PARAM(nonPawnStmCorrWeight, 316, 96, 768, 64);
+SEARCH_PARAM(nonPawnNstmCorrWeight, 277, 96, 768, 64);
+SEARCH_PARAM(threatsCorrWeight, 280, 96, 768, 64);
+SEARCH_PARAM(minorCorrWeight, 306, 96, 768, 64);
+SEARCH_PARAM(majorCorrWeight, 322, 96, 768, 64);
 
 SEARCH_PARAM(aspInitDelta, 9, 8, 30, 4);
 SEARCH_PARAM(minAspDepth, 5, 3, 7, 1);
