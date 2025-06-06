469ddc2f62f4138528c420c8082d920f421608c6
AspectOfTheNoob <Mcthouacbb@gmail.com>
2025-06-06 16:15:31 -0400

    Adjust RFP margin by correction value Bench: 6856639

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -498,7 +498,8 @@ int Search::search(SearchThread& thread, int depth, SearchStack* stack, int alph
     {
         // reverse futility pruning(~86 elo)
         int rfpMargin = (improving ? rfpImpMargin : rfpNonImpMargin) * depth
-            - rfpOppWorsening * oppWorsening + (stack - 1)->histScore / rfpHistDivisor;
+            - rfpOppWorsening * oppWorsening + (stack - 1)->histScore / rfpHistDivisor
+            + std::abs(correctionValue) / 400;
         if (depth <= rfpMaxDepth && stack->eval >= std::max(rfpMargin, 20) + beta)
             return stack->eval;
 
