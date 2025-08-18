2caf2262c0da1b702af591009b75643359c7e546
mcthouacbb <mcthouacbb@gmail.com>
2025-08-18 17:36:25 -0400

    Untested viriformat

---
+++ Sirius/src/datagen/viriformat.h
@@ -0,0 +1,25 @@
+#include "marlinformat.h"
+
+namespace viriformat
+{
+
+struct ViriMove
+{
+    explicit ViriMove(uint16_t data);
+    explicit ViriMove(Move move);
+    Move toMove() const;
+
+private:
+    uint16_t m_Data;
+};
+
+struct Game
+{
+    marlinformat::PackedBoard startpos;
+    std::vector<std::pair<ViriMove, int16_t>> moves;
+
+    static Game read(std::istream& is);
+    void write(std::ostream& os) const;
+};
+
+}
