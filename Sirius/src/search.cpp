09f2955f64f6b83e49f22a15557b37346ede43f7
mcthouacbb <mcthouacbb@gmail.com>
2025-08-30 09:40:00 -0400

    Store to TT in stand pat beta cutoff Bench: 6580042

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -909,7 +909,11 @@ int Search::qsearch(SearchThread& thread, SearchStack* stack, int alpha, int bet
     }
 
     if (stack->eval >= beta)
+    {
+        m_TT.store(board.zkey(), rootPly, 0, stack->eval, rawStaticEval, Move::nullmove(), ttPV,
+            TTEntry::Bound::LOWER_BOUND);
         return stack->eval;
+    }
     if (stack->eval > alpha)
         alpha = stack->eval;
 
