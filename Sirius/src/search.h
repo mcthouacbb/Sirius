77b4ef5159eae89c3536138e06cd41c5d60a693a
mcthouacbb <mcthouacbb@gmail.com>
2025-09-29 21:59:09 -0400

    Fix some more stuff Bench: 6168513

--- Sirius/src/search.h
+++ Sirius/src/search.h
@@ -157,7 +157,7 @@ private:
     void reportUCIInfo(const SearchThread& thread, int multiPVIdx, int depth) const;
 
     int iterDeep(SearchThread& thread, bool report);
-    int aspWindows(SearchThread& thread, int depth, Move& bestMove, int prevScore, bool report);
+    int aspWindows(SearchThread& thread, int depth, int prevScore, bool report);
 
     int search(SearchThread& thread, int depth, SearchStack* stack, int alpha, int beta,
         bool pvNode, bool cutnode);
