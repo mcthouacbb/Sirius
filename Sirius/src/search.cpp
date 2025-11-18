219c48b55a5ed45757253276fefb6b1eafda1ca4
mcthouacbb <mcthouacbb@gmail.com>
2025-11-18 09:02:39 -0500

    10 -> 20 Bench: 6384212

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -531,7 +531,7 @@ i32 Search::search(SearchThread& thread, i32 depth, SearchStack* stack, i32 alph
     if (!pvNode && !inCheck && !excluded)
     {
         // reverse futility pruning(~86 elo)
-        i32 rfpMargin = (improving ? rfpImpMargin + 10 * oppEasyCapture : rfpNonImpMargin) * depth
+        i32 rfpMargin = (improving ? rfpImpMargin + 20 * oppEasyCapture : rfpNonImpMargin) * depth
             - rfpOppWorsening * oppWorsening + (stack - 1)->histScore / rfpHistDivisor;
         if (depth <= rfpMaxDepth && stack->eval >= std::max(rfpMargin, 20) + beta)
             return stack->eval;
