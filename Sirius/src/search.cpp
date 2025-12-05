eca81879fcd3ac81422ca3ee1e094b169bbf0d88
mcthouacbb <mcthouacbb@gmail.com>
2025-12-04 21:35:12 -0500

    NMP eval >= beta + 30 Bench: 6437064

--- Sirius/src/search.cpp
+++ Sirius/src/search.cpp
@@ -549,7 +549,7 @@ i32 Search::search(SearchThread& thread, i32 depth, SearchStack* stack, i32 alph
         Bitboard nonPawns =
             board.pieces(board.sideToMove()) ^ board.pieces(board.sideToMove(), PieceType::PAWN);
         if (board.pliesFromNull() > 0 && rootPly >= thread.nmpMinPly && depth >= nmpMinDepth
-            && stack->eval >= beta
+            && stack->eval >= beta + 30
             && stack->staticEval >= beta + nmpEvalBaseMargin - nmpEvalDepthMargin * depth
             && nonPawns.multiple())
         {
