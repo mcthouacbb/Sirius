22a0f1b7cc0928f73334c5ca8d95394edc8a61bd
mcthouacbb <mcthouacbb@gmail.com>
2026-01-30 12:03:00 -0500

    Tuned for 400k games 25k iters Bench: 6365240

--- Sirius/src/search_params.h
+++ Sirius/src/search_params.h
@@ -5,8 +5,6 @@
 #include <functional>
 #include <string>
 
-#define EXTERNAL_TUNE
-
 #include "defs.h"
 
 namespace search
@@ -58,126 +56,126 @@ SEARCH_PARAM(bmStabilityScale, 969, 200, 1600, 40);
 SEARCH_PARAM(bmStabilityOffset, 266, 100, 1000, 40);
 SEARCH_PARAM(bmStabilityPower, -153, -250, -120, 8);
 
-SEARCH_PARAM(maxHistBonus, 2036, 512, 3072, 256);
-SEARCH_PARAM(histBonusQuadratic, 441, 1, 1536, 64);
-SEARCH_PARAM(histBonusLinear, 219, 64, 384, 32);
-SEARCH_PARAM(histBonusOffset, 98, -384, 768, 64);
+SEARCH_PARAM(maxHistBonus, 2116, 512, 3072, 256);
+SEARCH_PARAM(histBonusQuadratic, 407, 1, 1536, 64);
+SEARCH_PARAM(histBonusLinear, 229, 64, 384, 32);
+SEARCH_PARAM(histBonusOffset, -152, -384, 768, 64);
 
-SEARCH_PARAM(maxHistMalus, 1093, 512, 3072, 256);
-SEARCH_PARAM(histMalusQuadratic, 292, 1, 1536, 64);
-SEARCH_PARAM(histMalusLinear, 302, 64, 384, 32);
-SEARCH_PARAM(histMalusOffset, 27, -384, 768, 64);
+SEARCH_PARAM(maxHistMalus, 1317, 512, 3072, 256);
+SEARCH_PARAM(histMalusQuadratic, 320, 1, 1536, 64);
+SEARCH_PARAM(histMalusLinear, 372, 64, 384, 32);
+SEARCH_PARAM(histMalusOffset, -68, -384, 768, 64);
 
-SEARCH_PARAM(histBetaMargin, 39, 10, 120, 5);
+SEARCH_PARAM(histBetaMargin, 26, 10, 120, 5);
 
 // 256 corrhist units = 1 eval unit
-SEARCH_PARAM(maxCorrHist, 8091, 6144, 24576, 512);
-SEARCH_PARAM(maxCorrHistUpdate, 2009, 1024, 8192, 64);
+SEARCH_PARAM(maxCorrHist, 6684, 6144, 24576, 512);
+SEARCH_PARAM(maxCorrHistUpdate, 1700, 1024, 8192, 64);
 
-SEARCH_PARAM(pawnCorrWeight, 418, 96, 768, 64);
-SEARCH_PARAM(nonPawnStmCorrWeight, 387, 96, 768, 64);
-SEARCH_PARAM(nonPawnNstmCorrWeight, 273, 96, 768, 64);
-SEARCH_PARAM(threatsCorrWeight, 228, 96, 768, 64);
-SEARCH_PARAM(minorCorrWeight, 266, 96, 768, 64);
-SEARCH_PARAM(majorCorrWeight, 404, 96, 768, 64);
-SEARCH_PARAM(contCorr2Weight, 349, 96, 768, 64);
-SEARCH_PARAM(contCorr3Weight, 175, 96, 768, 64);
-SEARCH_PARAM(contCorr4Weight, 230, 96, 768, 64);
-SEARCH_PARAM(contCorr5Weight, 219, 96, 768, 64);
-SEARCH_PARAM(contCorr6Weight, 163, 96, 768, 64);
-SEARCH_PARAM(contCorr7Weight, 142, 96, 768, 64);
-SEARCH_PARAM(highCorrplexityMargin, 87, 40, 120, 5);
+SEARCH_PARAM(pawnCorrWeight, 258, 96, 768, 64);
+SEARCH_PARAM(nonPawnStmCorrWeight, 217, 96, 768, 64);
+SEARCH_PARAM(nonPawnNstmCorrWeight, 264, 96, 768, 64);
+SEARCH_PARAM(threatsCorrWeight, 204, 96, 768, 64);
+SEARCH_PARAM(minorCorrWeight, 235, 96, 768, 64);
+SEARCH_PARAM(majorCorrWeight, 269, 96, 768, 64);
+SEARCH_PARAM(contCorr2Weight, 354, 96, 768, 64);
+SEARCH_PARAM(contCorr3Weight, 182, 96, 768, 64);
+SEARCH_PARAM(contCorr4Weight, 186, 96, 768, 64);
+SEARCH_PARAM(contCorr5Weight, 409, 96, 768, 64);
+SEARCH_PARAM(contCorr6Weight, 119, 96, 768, 64);
+SEARCH_PARAM(contCorr7Weight, 187, 96, 768, 64);
+SEARCH_PARAM(highCorrplexityMargin, 76, 40, 120, 5);
 
-SEARCH_PARAM(aspInitDelta, 10, 5, 30, 2);
+SEARCH_PARAM(aspInitDelta, 16, 5, 30, 2);
 SEARCH_PARAM(minAspDepth, 6, 3, 7, 1);
-SEARCH_PARAM(aspWideningFactor, 58, 1, 512, 24);
+SEARCH_PARAM(aspWideningFactor, 133, 1, 512, 24);
 
 SEARCH_PARAM(minIIRDepth, 4, 2, 9, 1);
 
 SEARCH_PARAM(rfpMaxDepth, 8, 4, 10, 1);
-SEARCH_PARAM(rfpImpMargin, 27, 20, 80, 8);
-SEARCH_PARAM(rfpNonImpMargin, 78, 50, 100, 8);
-SEARCH_PARAM(rfpOppEasyCapture, 21, 5, 100, 8);
-SEARCH_PARAM(rfpOppWorsening, 14, 5, 100, 8);
-SEARCH_PARAM(rfpHistDivisor, 410, 256, 512, 16);
+SEARCH_PARAM(rfpImpMargin, 35, 20, 80, 8);
+SEARCH_PARAM(rfpNonImpMargin, 81, 50, 100, 8);
+SEARCH_PARAM(rfpOppEasyCapture, 24, 5, 100, 8);
+SEARCH_PARAM(rfpOppWorsening, 49, 5, 100, 8);
+SEARCH_PARAM(rfpHistDivisor, 420, 256, 512, 16);
 
 SEARCH_PARAM(razoringMaxDepth, 3, 1, 5, 1);
-SEARCH_PARAM(razoringMargin, 456, 250, 650, 10);
+SEARCH_PARAM(razoringMargin, 472, 250, 650, 10);
 
 SEARCH_PARAM(nmpMinDepth, 2, 2, 5, 1);
-SEARCH_PARAM(nmpEvalMargin, 30, 0, 100, 10);
-SEARCH_PARAM(nmpStaticEvalBaseMargin, 184, 50, 300, 10);
-SEARCH_PARAM(nmpStaticEvalDepthMargin, 19, 10, 50, 2);
-SEARCH_PARAM(nmpBaseReduction, 1320, 512, 2048, 64);
-SEARCH_PARAM(nmpDepthReductionScale, 74, 32, 96, 12);
-SEARCH_PARAM(nmpEvalReductionScale, 215, 50, 300, 10);
+SEARCH_PARAM(nmpEvalMargin, 31, 0, 100, 10);
+SEARCH_PARAM(nmpStaticEvalBaseMargin, 155, 50, 300, 10);
+SEARCH_PARAM(nmpStaticEvalDepthMargin, 28, 10, 50, 2);
+SEARCH_PARAM(nmpBaseReduction, 1461, 512, 2048, 64);
+SEARCH_PARAM(nmpDepthReductionScale, 56, 32, 96, 12);
+SEARCH_PARAM(nmpEvalReductionScale, 194, 50, 300, 10);
 SEARCH_PARAM(nmpMaxEvalReduction, 4, 2, 5, 1);
 
 SEARCH_PARAM(probcutMinDepth, 5, 4, 7, 1);
-SEARCH_PARAM(probcutBetaMargin, 190, 100, 300, 15);
+SEARCH_PARAM(probcutBetaMargin, 181, 100, 300, 15);
 SEARCH_PARAM(probcutReduction, 4, 3, 6, 1);
 
-SEARCH_PARAM(mvvPawn, 964, -20000, 20000, 200);
-SEARCH_PARAM(mvvKnight, 2465, -20000, 20000, 200);
-SEARCH_PARAM(mvvBishop, 2360, -20000, 20000, 200);
-SEARCH_PARAM(mvvRook, 4725, -20000, 20000, 200);
-SEARCH_PARAM(mvvQueen, 7181, -20000, 20000, 200);
+SEARCH_PARAM(mvvPawn, 922, -20000, 20000, 200);
+SEARCH_PARAM(mvvKnight, 2732, -20000, 20000, 200);
+SEARCH_PARAM(mvvBishop, 2192, -20000, 20000, 200);
+SEARCH_PARAM(mvvRook, 4427, -20000, 20000, 200);
+SEARCH_PARAM(mvvQueen, 7304, -20000, 20000, 200);
 
 SEARCH_PARAM(fpMaxDepth, 8, 4, 9, 1);
-SEARCH_PARAM(fpBaseMargin, 146, 60, 360, 12);
-SEARCH_PARAM(fpDepthMargin, 128, 10, 180, 12);
-SEARCH_PARAM(fpHistDivisor, 393, 256, 512, 16);
+SEARCH_PARAM(fpBaseMargin, 120, 60, 360, 12);
+SEARCH_PARAM(fpDepthMargin, 83, 10, 180, 12);
+SEARCH_PARAM(fpHistDivisor, 410, 256, 512, 16);
 
 SEARCH_PARAM(noisyFpMaxDepth, 5, 2, 9, 1);
-SEARCH_PARAM(noisyFPBaseMargin, 4, -100, 200, 10);
-SEARCH_PARAM(noisyFpDepthMargin, 113, 10, 360, 12);
-SEARCH_PARAM(noisyFpHistDivisor, 253, 100, 512, 16);
+SEARCH_PARAM(noisyFPBaseMargin, 12, -100, 200, 10);
+SEARCH_PARAM(noisyFpDepthMargin, 115, 10, 360, 12);
+SEARCH_PARAM(noisyFpHistDivisor, 304, 100, 512, 16);
 
-SEARCH_PARAM(lmpImpBase, 553, 128, 2048, 64);
-SEARCH_PARAM(lmpImpDepth, 333, 64, 512, 48);
-SEARCH_PARAM(lmpNonImpBase, 566, 128, 2048, 64);
-SEARCH_PARAM(lmpNonImpDepth, 103, 64, 512, 48);
+SEARCH_PARAM(lmpImpBase, 905, 128, 2048, 64);
+SEARCH_PARAM(lmpImpDepth, 469, 64, 512, 48);
+SEARCH_PARAM(lmpNonImpBase, 764, 128, 2048, 64);
+SEARCH_PARAM(lmpNonImpDepth, 121, 64, 512, 48);
 
-SEARCH_PARAM(seePruneMarginNoisy, -96, -120, -30, 6);
-SEARCH_PARAM(seePruneMarginQuiet, -67, -120, -30, 6);
-SEARCH_PARAM(seeCaptHistMax, 103, 50, 200, 6);
-SEARCH_PARAM(seeCaptHistDivisor, 30, 16, 96, 2);
+SEARCH_PARAM(seePruneMarginNoisy, -114, -120, -30, 6);
+SEARCH_PARAM(seePruneMarginQuiet, -75, -120, -30, 6);
+SEARCH_PARAM(seeCaptHistMax, 99, 50, 200, 6);
+SEARCH_PARAM(seeCaptHistDivisor, 34, 16, 96, 2);
 
 SEARCH_PARAM(maxHistPruningDepth, 7, 2, 8, 1);
-SEARCH_PARAM(histPruningMargin, 1743, 512, 4096, 128);
+SEARCH_PARAM(histPruningMargin, 2093, 512, 4096, 128);
 
 SEARCH_PARAM(seMinDepth, 5, 4, 9, 1);
 SEARCH_PARAM(seTTDepthMargin, 3, 2, 5, 1);
-SEARCH_PARAM(sBetaScale, 55, 24, 128, 4);
+SEARCH_PARAM(sBetaScale, 64, 24, 128, 4);
 SEARCH_PARAM(sBetaScaleFormerPV, 20, 10, 128, 4);
-SEARCH_PARAM(doubleExtMargin, 11, 0, 40, 2);
-SEARCH_PARAM(tripleExtMargin, 125, 0, 200, 8);
+SEARCH_PARAM(doubleExtMargin, 12, 0, 40, 2);
+SEARCH_PARAM(tripleExtMargin, 110, 0, 200, 8);
 
 SEARCH_PARAM(lmrMinDepth, 3, 2, 5, 1);
 SEARCH_PARAM(lmrMinMovesNonPv, 3, 1, 6, 1);
 SEARCH_PARAM(lmrMinMovesPv, 4, 2, 8, 1);
 SEARCH_PARAM(lmrFailHighCountMargin, 2, 2, 12, 1);
 
-SEARCH_PARAM_CALLBACK(lmrBase, 775, -512, 2048, 110, updateLmrTable);
-SEARCH_PARAM_CALLBACK(lmrScale, 427, 300, 600, 50, updateLmrTable);
-SEARCH_PARAM(lmrQuietHistDivisor, 9043, 4096, 16384, 512);
-SEARCH_PARAM(lmrNoisyHistDivisor, 6598, 2048, 16384, 512);
+SEARCH_PARAM_CALLBACK(lmrBase, 887, -512, 2048, 110, updateLmrTable);
+SEARCH_PARAM_CALLBACK(lmrScale, 433, 300, 600, 50, updateLmrTable);
+SEARCH_PARAM(lmrQuietHistDivisor, 8287, 4096, 16384, 512);
+SEARCH_PARAM(lmrNoisyHistDivisor, 7081, 2048, 16384, 512);
 
-SEARCH_PARAM(lmrNonImp, 1478, 0, 2048, 256);
-SEARCH_PARAM(lmrNoisyTTMove, 1082, 0, 2048, 256);
-SEARCH_PARAM(lmrTTPV, 954, 0, 2048, 256);
-SEARCH_PARAM(lmrTTPVNonFailLow, 484, 0, 2048, 256);
-SEARCH_PARAM(lmrGivesCheck, 573, 0, 2048, 256);
-SEARCH_PARAM(lmrInCheck, 592, 0, 2048, 256);
-SEARCH_PARAM(lmrCorrplexity, 593, 0, 2048, 256);
-SEARCH_PARAM(lmrCutnode, 1612, 0, 2048, 256);
-SEARCH_PARAM(lmrFailHighCount, 1042, 0, 2048, 256);
+SEARCH_PARAM(lmrNonImp, 1064, 0, 2048, 256);
+SEARCH_PARAM(lmrNoisyTTMove, 1256, 0, 2048, 256);
+SEARCH_PARAM(lmrTTPV, 1595, 0, 2048, 256);
+SEARCH_PARAM(lmrTTPVNonFailLow, 884, 0, 2048, 256);
+SEARCH_PARAM(lmrGivesCheck, 238, 0, 2048, 256);
+SEARCH_PARAM(lmrInCheck, 9, 0, 2048, 256);
+SEARCH_PARAM(lmrCorrplexity, 596, 0, 2048, 256);
+SEARCH_PARAM(lmrCutnode, 1932, 0, 2048, 256);
+SEARCH_PARAM(lmrFailHighCount, 1224, 0, 2048, 256);
 
-SEARCH_PARAM(doDeeperMarginBase, 38, 15, 55, 5);
-SEARCH_PARAM(doDeeperMarginDepth, 143, 32, 384, 16);
+SEARCH_PARAM(doDeeperMarginBase, 50, 15, 55, 5);
+SEARCH_PARAM(doDeeperMarginDepth, 157, 32, 384, 16);
 
-SEARCH_PARAM(doShallowerMargin, 8, 2, 15, 1);
+SEARCH_PARAM(doShallowerMargin, 7, 2, 15, 1);
 
-SEARCH_PARAM(qsFpMargin, 80, 0, 250, 16);
+SEARCH_PARAM(qsFpMargin, 85, 0, 250, 16);
 
 }
